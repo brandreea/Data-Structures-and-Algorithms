243f3dd854e5cf3eac5a382a835c30045a79e148
brandreea <49251096+brandreea@users.noreply.github.com>
2020-01-10 11:31:07 +0200

    Update Problema cu maxim.cpp

--- TAP/Dinamic Programming/Problema cu maxim/Problema cu maxim.cpp
+++ TAP/Dinamic Programming/Problema cu maxim/Problema cu maxim.cpp
@@ -16,9 +16,10 @@ int v[NMAX + 5];
 ifstream fin("data.in");
 int isMaxim[NMAX + 5][NMAX + 5];
 int longestDecSeq[NMAX + 5];
-//1->este maxim in secventa de lungime maxima
-//2->nu este
-//3->exista cate o secventa de lungime maxima pentru ambele
+//1->the only maximum length seqences ending in v[i] have v[i] as maximum point
+//2->the only maximum length sequences ending in v[i] do not have it as maximum point
+//3->we have found two distinct sequences, ine for which v[i] is maximum point
+//  and one for which it isn't, both of maximum length
 
 int D[NMAX + 5][NMAX + 5];
 void read()
@@ -31,7 +32,9 @@ void dpUtil()
 {
 	//yet another dp ::::)
 	//at least works O(n^2) and doesn't ruin complexity
-	//a dp subproblem for finding the longest increasing increasing sequence that has v[i] as only maximum point 
+	//a dp subproblem for finding the longest increasing sequence that has v[i] as only maximum point 
+        //this function is only used in initialising the first column of the matrix
+        
 	longestDecSeq[0] = 1;
 	for (int i = 1; i < n; i++)
 		for (int j = 0; j < i; j++)
